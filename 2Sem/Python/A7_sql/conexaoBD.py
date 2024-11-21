f9f9e0be51d665a3612994f7adda6e8b18e41d33
Bernardo27dev <caiobernardo.dev@outlook.com>
2024-11-21 00:37:01 -0300

    feat: functional code

--- 2Sem/Python/A7_sql/conexaoBD.py
+++ 2Sem/Python/A7_sql/conexaoBD.py
@@ -10,7 +10,15 @@ try:
     username = 'sa'
     password = '*123456HAS*'
     # Conexão
-    conn = pyodbc.connect('DRIVER={SQL Server}; SERVER=' + server + ';DATABSE=' + database + ';UID=' + username + ';PWD=' + password)
+    conn = pyodbc.connect('DRIVER={SQL Server};'
+        'SERVER=pets_database.mssql.somee.com;'
+        'DATABASE=pets_database;'
+        'Uid=Vini_dev_SQLLogin_1;'
+        'PWD=kov33gmdsc;'
+        'TrustServerCertificate=True;'
+        'Persist Security Info=False;'
+        'Packet Size=4096;')
+    # conn = pyodbc.connect('DRIVER={SQL Server}; SERVER=' + server + ';DATABASE=' + database + ';Uid=' + username + ';PWD=' + password)
     inst_cadastro = conn.cursor()
     inst_consulta = conn.cursor()
 except Exception as e:
@@ -28,6 +36,9 @@ while conexao:
     0 - Sair
     1 - Cadastrar Pet
     2 - Listar Pets
+    3 - Buscar Pets
+    4 - Editar Pet
+    5 - Excluir Pet
     """)
     escolha = int(input("Escolha: "))
     system("cls")
@@ -39,7 +50,7 @@ while conexao:
                 print("CADASTRANDO PET")
                 tipo = input("Tipo.............")
                 nome = input("Nome.............")
-                idade = int(input("Idade............."))
+                idade = int(input("idade............."))
                 cadastro = f"""
                 INSERT INTO petshop (tipo_pet, nome_pet, idade)
                 VALUES ('{tipo}','{nome}',{idade})
@@ -60,13 +71,81 @@ while conexao:
             lista_dados = sorted(lista_dados)
 
             dados_df = pd.DataFrame.from_records(lista_dados,
-                                                 columns=['Id', 'Tipo', 'Nome', 'Idade'], index='Id')
+                                                 columns=['id', 'Tipo', 'Nome', 'idade'], index='id')
             
             if dados_df.empty:
                 print("Sem dados")
             else:
                 print(dados_df)
-        
+        case 3:
+            lista_dados = []
+            print('Buscar cadastro')
+            idPet = int(input("id: "))
+            inst_consulta.execute(f"select * from petshop where id = {idPet}")
+            data = inst_consulta.fetchall()  
+            
+            if data == "" or data == None:
+                print("Sem dados")
+            else:
+                for dt in data:
+                    lista_dados.append(dt)
+                lista_dados = sorted(lista_dados)
+                dados_df = pd.DataFrame.from_records(lista_dados, columns=['id', 'Tipo', 'Nome', 'idade'], index='id')
+                print(dados_df)
+        case 4:
+            lista_dados = []
+            print('Buscar cadastro')
+            idPet = int(input("id: "))
+            inst_consulta.execute(f"select * from petshop where id = {idPet}")
+            data = inst_consulta.fetchall()  
 
+            
+            
+            if data == "" or data == None:
+                print("Sem dados")
+            else:
+                for dt in data:
+                    lista_dados.append(dt)
+                lista_dados = sorted(lista_dados)
+                dados_df = pd.DataFrame.from_records(lista_dados, columns=['id', 'Tipo', 'Nome', 'idade'], index='id')
+                try:
+                    print(f"Dados Anteriores:\n{dados_df}")
+                    print("EDITANDO PET")
+                    tipo = input("Tipo.............")
+                    nome = input("Nome.............")
+                    idade = int(input("idade............."))
+                    print(tipo)
+                    edit = f"""
+                    UPDATE petshop
+                    SET tipo_pet = '{tipo}', nome_pet = '{nome}', idade = {idade} WHERE id = {idPet}
+                    """
+                    inst_cadastro.execute(edit)
+                    conn.commit()
+                except ValueError:
+                    print("idade deve ser numerico")
+                else:
+                    print("Editado com sucesso!")
+        case 5:
+            print('Deletar cadastro')
+            idPet = int(input("id: "))
+            inst_consulta.execute(f"select nome_pet from petshop where id = {idPet}")
+            data = inst_consulta.fetchone()    
 
+            if data == "" or data == None:
+                print("Sem dados")
+            else:
+                while True:
+                    opt = str(input(f"Deseja deletar o cadastro do {data[0]} Y/N"))
+                    match opt.lower():
+                        case 'y':
+                            inst_consulta.execute(f"delete from petshop where id = {idPet}")
+                            print("Deletado.")
+                            break
+                        case 'n':
+                            print("Canncelado.")
+                            break
+                        case _:
+                            print("Opção Inválida.")
+        case _:
+            print("Escolha uma opção válida.")
     input("Pressione qualquer tecla para continuar")
